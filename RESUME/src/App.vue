0f3ec61565501c629ab18db1d0a442982b2b6462
Nathann001 <nathanielsales457@gmail.com>
2024-10-01 00:44:11 +0800

    Add files via upload

---
+++ RESUME/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>My Resume</h1>
+      <h2>Sales, Nathaniel S.</h2>
+      <p>I'm a website developer looking for a full-time job where I can use my skills in building and improving websites. I focus on creating sites that are easy to use and help businesses succeed. I also have experience working with teams, managing projects, and finding ways to make processes smoother. My goal is to deliver high-quality websites that work well and meet deadlines.</p>
+    </header>
+
+    <section>
+      <h2>Projects</h2>
+      <ul>
+        <li>Worked with a team of 4 to design and develop a dynamic website featuring the schedules, standings, and news for FIA races, including F1 and WEC. Improved user engagement by 25% over the first two weeks of launch.</li>
+        <li>Independently designed and developed a website which showcases different motorcycles featuring news, reviews, and blog posts.</li>
+      </ul>
+    </section>
+
+    <section>
+      <h2>Skills</h2>
+      <ul>
+        <li>Communication</li>
+        <li>Teamwork</li>
+        <li>Problem-Solving</li>
+      </ul>
+    </section>
+
+    <section>
+      <h2>Education</h2>
+      <ul>
+        <li>Holy Angel University - Bachelor of Information and Technology (Currently in 3rd Year)</li>
+        <li>Don Bosco Academy - High School Diploma (2021)</li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+header {
+  margin-bottom: 30px;
+}
+
+section {
+  margin: 20px 0;
+}
+
+h1 {
+  color: #42b983;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+ul li {
+  margin: 10px 0;
+}
+</style>
